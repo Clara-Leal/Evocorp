ca47d633826681399e45fe033defa122b8c827d9
Clara-Leal <81273768+Clara-Leal@users.noreply.github.com>
2021-09-15 22:09:17 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace EvoCorp
+{
+	partial class frmlogin
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmlogin));
+            this.btningresar = new System.Windows.Forms.Button();
+            this.txbcontraseña = new System.Windows.Forms.TextBox();
+            this.txbusuario = new System.Windows.Forms.TextBox();
+            this.lblcontraseña = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btningresar
+            // 
+            this.btningresar.Location = new System.Drawing.Point(21, 245);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(75, 23);
+            this.btningresar.TabIndex = 0;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = true;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // txbcontraseña
+            // 
+            this.txbcontraseña.Location = new System.Drawing.Point(21, 195);
+            this.txbcontraseña.Name = "txbcontraseña";
+            this.txbcontraseña.Size = new System.Drawing.Size(144, 23);
+            this.txbcontraseña.TabIndex = 1;
+            // 
+            // txbusuario
+            // 
+            this.txbusuario.Location = new System.Drawing.Point(21, 130);
+            this.txbusuario.Name = "txbusuario";
+            this.txbusuario.Size = new System.Drawing.Size(144, 23);
+            this.txbusuario.TabIndex = 2;
+            // 
+            // lblcontraseña
+            // 
+            this.lblcontraseña.AutoSize = true;
+            this.lblcontraseña.Location = new System.Drawing.Point(21, 174);
+            this.lblcontraseña.Name = "lblcontraseña";
+            this.lblcontraseña.Size = new System.Drawing.Size(67, 15);
+            this.lblcontraseña.TabIndex = 3;
+            this.lblcontraseña.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Usuario";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 301);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblcontraseña);
+            this.Controls.Add(this.txbusuario);
+            this.Controls.Add(this.txbcontraseña);
+            this.Controls.Add(this.btningresar);
+            this.Name = "frmlogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btningresar;
+		private System.Windows.Forms.TextBox txbcontraseña;
+		private System.Windows.Forms.TextBox txbusuario;
+		private System.Windows.Forms.Label lblcontraseña;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
+namespace EvoCorp
+
+{
+	public partial class frmlogin : Form
+	{
+		public frmlogin()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btningresar_Click(object sender, EventArgs e)
+		{
+
+			//String usuarioEncriptado = encriptacion.((txbcontraseña.Text);
+			try
+			{
+
+				MySqlConnection conexion = new MySqlConnection("server=localhost; database=la_rubia;Uid=usuario1; pwd = evocorp;");
+
+				conexion.Open();
+
+				MySqlCommand consulta = new MySqlCommand();
+
+				MySqlConnection conectarnos = new MySqlConnection();
+
+				consulta.Connection = conexion;
+
+				consulta.CommandText = ("select *from login where usuario= '" + txbusuario.Text + "' AND contraseña= '" + txbcontraseña.Text + "' ");
+
+				MySqlDataReader datos = consulta.ExecuteReader();
+
+				if (datos.Read())
+				{
+					MessageBox.Show("Acceso correcto" + txbusuario.Text);
+					/*frmlogin inicio = new frmlogin();
+					inicio.Show();
+					this.Hide();*/
+				}
+				else
+				{
+					MessageBox.Show("Acceso denegado");
+				}
+
+			}catch (Exception ex)
+			
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+	}
+}
